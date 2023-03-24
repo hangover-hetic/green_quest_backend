621527ebe5849eb97c30667c48f3b35cd3d8dc80
Jessy <ernatusjessy@gmail.com>
2023-03-24 11:59:14 +0100

    add events

--- greenquest/src/DataFixtures/AppFixtures.php
+++ greenquest/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for( $i = 0; $i < 10; $i++ ){
+            $event = new Event();
+            $event->setTitle( "Title", $i ); 
+            $event->setDescription( "Description", $i );
+            $event->setPosition( [12, 20, 50] );
+            $manager->persist($event);
+        }
 
         $manager->flush();
     }
---
+++ greenquest/src/Entity/Event.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection()
+    ]
+)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $position = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPosition(): array
+    {
+        return $this->position;
+    }
+
+    public function setPosition(array $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+}
