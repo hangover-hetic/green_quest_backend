99b211a7868185ade68b3968d5084845dadefc83
Jessy <ernatusjessy@gmail.com>
2023-03-31 11:20:20 +0200

    fix migrations

--- greenquest/migrations/Version20230331091653.php
+++ greenquest/migrations/Version20230331091653.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230324111637 extends AbstractMigration
+final class Version20230331091653 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,12 @@ final class Version20230324111637 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event ADD longitude DOUBLE PRECISION NOT NULL');
-        $this->addSql('ALTER TABLE event ADD latitude DOUBLE PRECISION NOT NULL');
-        $this->addSql('ALTER TABLE event DROP "position"');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE event ADD "position" TEXT NOT NULL');
-        $this->addSql('ALTER TABLE event DROP longitude');
-        $this->addSql('ALTER TABLE event DROP latitude');
-        $this->addSql('COMMENT ON COLUMN event."position" IS \'(DC2Type:array)\'');
     }
 }
