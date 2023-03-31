485401f4f8cc4bbb289a54b2cd41d5f3244eb082
Adrien <adrien.bouteiller01@gmail.com>
2023-03-31 10:08:12 +0200

    login_check_finish

--- greenquest/src/DataFixtures/AppFixtures.php
+++ greenquest/src/DataFixtures/AppFixtures.php
@@ -5,9 +5,16 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private readonly UserPasswordHasherInterface $passwordEncoder)
+    {
+
+    }
+
     public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 10; $i++) {
@@ -15,7 +22,7 @@ class AppFixtures extends Fixture
             $user->setEmail('user' . $i . '@example.com');
             $user->setFirstname('User' . $i);
             $user->setLastname('User' . $i);
-            $user->setPassword('password');
+            $user->setPassword($this->passwordEncoder->hashPassword($user, 'password'));
             $user->setExp(0);
             $user->setBlobs(0);
             $user->setRoles(['ROLE_USER']);
