df54f5df3e709107649eb397f82b3ae05dd0dc77
Adrien <adrien.bouteiller01@gmail.com>
2023-03-31 09:47:20 +0200

    login_check

--- greenquest/src/DataFixtures/AppFixtures.php
+++ greenquest/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail('user' . $i . '@example.com');
+            $user->setFirstname('User' . $i);
+            $user->setLastname('User' . $i);
+            $user->setPassword('password');
+            $user->setExp(0);
+            $user->setBlobs(0);
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
