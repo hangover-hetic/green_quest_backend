1178003a793484297ca766d0d6646616dc32923f
Adrien <59483650+adrien24@users.noreply.github.com>
2023-03-31 10:20:35 +0200

    Login (#2)
    * login_check
    
    * login_check_finish
    
    ---------
    
    Co-authored-by: Piero Neri <49958632+PieroNr@users.noreply.github.com>

--- greenquest/src/DataFixtures/AppFixtures.php
+++ greenquest/src/DataFixtures/AppFixtures.php
@@ -2,14 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private readonly UserPasswordHasherInterface $passwordEncoder)
+    {
+
+    }
+
     public function load(ObjectManager $manager): void
     {
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail('user' . $i . '@example.com');
+            $user->setFirstname('User' . $i);
+            $user->setLastname('User' . $i);
+            $user->setPassword($this->passwordEncoder->hashPassword($user, 'password'));
+            $user->setExp(0);
+            $user->setBlobs(0);
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+        }
+        
         for( $i = 0; $i < 10; $i++ ){
             $event = new Event();
             $event->setTitle( "Title", $i ); 
