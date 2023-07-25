af89c888de887b9952aebe37857abc7f7667f804
Timothée DURAND <timothee.durand70@gmail.com>
2023-07-25 13:28:15 +0200

    fix: migrations

--- greenquest/migrations/Version20230725102910.php
+++ greenquest/migrations/Version20230725102910.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230724125157 extends AbstractMigration
+final class Version20230725102910 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,10 +20,8 @@ final class Version20230724125157 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE me_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE category (id INT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('DROP TABLE me');
         $this->addSql('ALTER TABLE event ADD category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA712469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_3BAE0AA712469DE2 ON event (category_id)');
@@ -35,8 +33,6 @@ final class Version20230724125157 extends AbstractMigration
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE event DROP CONSTRAINT FK_3BAE0AA712469DE2');
         $this->addSql('DROP SEQUENCE category_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE me_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE me (id INT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP INDEX IDX_3BAE0AA712469DE2');
         $this->addSql('ALTER TABLE event DROP category_id');
